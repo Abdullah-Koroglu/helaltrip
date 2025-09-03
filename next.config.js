944f2957bbe6659f6314260b57b887f4b4550c42
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2025-09-03 17:41:20 +0300

    export

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: process.env.NODE_ENV !== 'DEV' ? 'export' : 'standalone',
+  images: {
+    domains: ['images.unsplash.com', 'img.youtube.com'],
+  },
+}
+
+module.exports = nextConfig 
