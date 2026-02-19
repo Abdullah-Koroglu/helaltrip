c425d445ae16870dfaf490ea1629b88bf0d87519
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2026-02-19 14:42:23 +0300

    campaign order

--- lib/campaign-data.ts
+++ lib/campaign-data.ts
@@ -2,6 +2,11 @@
 
 export const campaigns = [
   {
+    id: "womeDeluxe",
+    image: "/hotelImages/wome_1.jpg",
+    link: "https://wa.me/905338189958?text=Wome Deluxe Grup fiyatlarını öğrenebilir miyim?"
+  },
+  {
     id: "earlyBooking",
     image: "/hotelImages/theoba_1.jpg",
     link: "https://wa.me/905338189958?text=Erken Dönem Rezervasyonları ile ilgili bilgi alabilir miyim?"
@@ -11,9 +16,4 @@ export const campaigns = [
     image: "/hotelImages/sahınn_1.jpg",
     link: "https://wa.me/905338189958?text=Şah Inn Paradise ile ilgili bilgi alabilir miyim?"
   },
-  {
-    id: "womeDeluxe",
-    image: "/hotelImages/wome_1.jpg",
-    link: "https://wa.me/905338189958?text=Wome Deluxe Grup fiyatlarını öğrenebilir miyim?"
-  }
 ];
